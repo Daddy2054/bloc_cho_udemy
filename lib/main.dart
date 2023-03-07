31bf92264d27721b18dc0dd50fe33aa01ad559d4
Jean <acalto2@gmail.com>
2023-03-07 17:06:56 +0100

    40

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_cubit/cubits/active_todo_count/active_todo_count_cubit.dart';
+import 'package:todo_cubit/cubits/filtered_todos/filtered_todos_cubit.dart';
+import 'package:todo_cubit/cubits/todo_filter/todo_filter_cubit.dart';
+import 'package:todo_cubit/cubits/todo_list/todo_list_cubit.dart';
+import 'package:todo_cubit/cubits/todo_search/todo_search_cubit.dart';
 
 import 'pages/todos_page.dart';
 
@@ -11,13 +17,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TodoFilterCubit>(
+          create: (context) => TodoFilterCubit(),
+        ),
+        BlocProvider<TodoSearchCubit>(
+          create: (context) => TodoSearchCubit(),
+        ),
+        BlocProvider<TodoListCubit>(
+          create: (context) => TodoListCubit(),
+        ),
+        BlocProvider<ActiveTodoCountCubit>(
+          create: (context) => ActiveTodoCountCubit(
+            todoListCubit: BlocProvider.of<TodoListCubit>(context),
+          ),
+        ),  BlocProvider<FilteredTodosCubit>(
+          create: (context) => FilteredTodosCubit(
+            todoFilterCubit: BlocProvider.of<TodoFilterCubit>(context),
+            todoSearchCubit: BlocProvider.of<TodoSearchCubit>(context),
+            todoListCubit: BlocProvider.of<TodoListCubit>(context),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODO',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodosPage(),
       ),
-      home: TodosPage(),
     );
   }
 }
