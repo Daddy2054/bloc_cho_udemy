92672ee46c1e28f7eabb4da058a1db61966d9d50
Jean <acalto2@gmail.com>
2023-03-11 00:34:11 +0100

    62

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'pages/home_page.dart';
-
+ 
 void main() async {
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: 'assets/.env');
   runApp(const MyApp());
 }
 
