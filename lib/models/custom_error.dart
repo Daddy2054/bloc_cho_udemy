bf471f2ed24cb9d19ef942a345b352410a50d831
Jean <acalto2@gmail.com>
2023-03-10 22:49:01 +0100

    59

---
+++ lib/models/custom_error.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class CustomError extends Equatable {
+  final String errMsg;
+  CustomError({
+    required this.errMsg,
+  });
+  
+
+  @override
+  List<Object> get props => [errMsg];
+
+  @override
+  bool get stringify => true;
+}
+
