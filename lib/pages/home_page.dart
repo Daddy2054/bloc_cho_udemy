fb3393d3e871b753f207327ccf503c5cecdaa60b
Jean <acalto2@gmail.com>
2023-03-11 23:01:29 +0100

    64

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:open_weather_cubit/pages/search_page.dart';
 import '/cubits/weather/weather_cubit.dart';
 import '/repositories/weather_repository.dart';
 import '/services/weather_api_services.dart';
@@ -14,6 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
   // @override
   // void initState() {
   //   super.initState();
@@ -35,6 +37,25 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () async {
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SearchPage();
+                  },
+                ),
+              );
+              print('city: $_city ');
+              if (_city != null) {
+                context.read<WeatherCubit>().fetchWeather(_city!);
+              }
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text('Home'),
