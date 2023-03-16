de87cf1b6a659e98f62cf86acd099a4a4f5f2b0b
Jean <acalto2@gmail.com>
2023-03-16 23:55:11 +0100

    85

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
+import 'package:fb_auth_bloc/pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
-    static const String routeName = '/home';
+  static const String routeName = '/home';
   const HomePage({super.key});
 
   @override
@@ -12,10 +15,53 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: ()  async => false,
+      onWillPop: () async => false,
       child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Home'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ProfilePage();
+                    },
+                  ),
+                );
+              },
+              icon: Icon(Icons.account_circle),
+            ),
+            IconButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(SignoutRequestedEvent());
+              },
+              icon: Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Home'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/bloc_logo_full.png',
+                width: MediaQuery.of(context).size.width * 0.8,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Bloc is an awesome\nstate management library\nfor Flutter!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
